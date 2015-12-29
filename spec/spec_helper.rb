3e5f0eecad33c33baddcfe322f02a6a5c4c8d2a9
William Eisert <weisert@eisertdev.com>
2015-12-29 17:58:46 +0000

    Code Climate test reporter added.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'codeclimate-test-reporter'
 require 'yapfac'
+
+CodeClimate::TestReporter.start
 
 class Fuubar
   DEFAULT_PROGRESS_BAR_OPTIONS[:format] = ' %c/%C [%w >:| %i] %e '
