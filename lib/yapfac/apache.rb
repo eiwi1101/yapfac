ac448da3bb16ae2f9d5af1aaba5aac59be42a622
William Eisert <weisert@eisertdev.com>
2015-12-29 19:10:05 +0000

    Added #load_site to Yapfac::Apache

--- lib/yapfac/apache.rb
+++ lib/yapfac/apache.rb
@@ -13,11 +13,18 @@ class Apache
   end
 
   def self.sites_available
-    Dir[File.join(Yapfac.configuration.apache_path, Yapfac.configuration.sites_available_path, '*.conf')]
+    Dir[File.join(Yapfac.configuration.apache_path, Yapfac.configuration.sites_available_path, '*.conf')].collect { |f| File.basename(f, '.conf') }
   end
 
   def self.sites_enabled
-    Dir[File.join(Yapfac.configuration.apache_path, Yapfac.configuration.sites_enabled_path, '*.conf')]
+    Dir[File.join(Yapfac.configuration.apache_path, Yapfac.configuration.sites_enabled_path, '*.conf')].collect { |f| File.basename(f, '.conf') }
+  end
+
+  def self.load_site(site_name)
+    file = File.join(Yapfac.configuration.apache_path, Yapfac.configuration.sites_available_path, site_name + '.conf')
+
+    return nil unless File.exists? file
+    return Yapfac::Apache::Site.load(file)
   end
 
 end
