d719149ab23e0bc2b8a9b25033393fe79457ac4f
William Eisert <weisert@eisertdev.com>
2015-12-29 19:13:01 +0000

    Updated spec to test configuration a bit more.

--- spec/yapfac_spec.rb
+++ spec/yapfac_spec.rb
@@ -5,6 +5,14 @@ describe Yapfac do
     expect(Yapfac::VERSION).not_to be nil
   end
 
+  it { is_expected.to respond_to :configuration }
+  it { is_expected.to respond_to :configure }
+
+  describe "#configuration" do
+    subject { Yapfac.configuration }
+    it { is_expected.to respond_to :reset! }
+  end
+
   describe "#configuration.apache_path" do
     subject { Yapfac.configuration.apache_path }
     it { is_expected.to eq "/etc/apache2" }
@@ -17,6 +25,14 @@ describe Yapfac do
       end
 
       it { is_expected.to eq "/foo/bar" }
+    end
+
+    context "after reset" do
+      before do
+        Yapfac.configuration.reset!
+      end
+
+      it { is_expected.to eq "/etc/apache2" }
     end
   end
 end
