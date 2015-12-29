5b29814267cd64188f5fad599888c116d2af0d32
William Eisert <weisert@eisertdev.com>
2015-12-29 18:06:30 +0000

    Changed order of spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+# CodeClimate Test Coverage
 require 'codeclimate-test-reporter'
-require 'yapfac'
-
 CodeClimate::TestReporter.start
+
+# Begin Local Things
+require 'yapfac'
 
 class Fuubar
   DEFAULT_PROGRESS_BAR_OPTIONS[:format] = ' %c/%C [%w >:| %i] %e '
