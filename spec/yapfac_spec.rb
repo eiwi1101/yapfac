30a998e0783adc8a76826867c15f326d8ebea19e
William Eisert <weisert@eisertdev.com>
2015-12-31 00:09:15 +0000

    Testing configuration direct assignment.

--- spec/yapfac_spec.rb
+++ spec/yapfac_spec.rb
@@ -5,12 +5,30 @@ describe Yapfac do
     expect(Yapfac::VERSION).not_to be nil
   end
 
+  it { is_expected.to respond_to :configuration= }
   it { is_expected.to respond_to :configuration }
   it { is_expected.to respond_to :configure }
 
   describe "#configuration" do
     subject { Yapfac.configuration }
     it { is_expected.to respond_to :reset! }
+  end
+
+  describe "#configuration=" do
+    subject(:new_config) { Yapfac::Configuration.new }
+    it { expect(Yapfac.configuration.apache_path).to eq "/etc/apache2" }
+
+    context "after config assignment" do
+      before do
+        new_config.apache_path = "/new/config"
+        Yapfac.configuration = new_config 
+      end
+
+      after { Yapfac.configuration.reset! }
+
+      it { expect(new_config.apache_path).to eq "/new/config" }
+      it { expect(Yapfac.configuration.apache_path).to eq "/new/config" }
+    end
   end
 
   describe "#configuration.apache_path" do
